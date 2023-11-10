ab8255f9370367391ca1d51a6df48ec7d2aff6af
1950019 <priyansiparida@gmail.com>
2023-11-10 13:00:42 +0530

    sbv complete

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
 from typing import Any
+from django.urls import reverse_lazy
 from django.shortcuts import render
-from django.views.generic import View, TemplateView, ListView, DetailView
+from django.views.generic import (View, TemplateView, 
+                                  ListView, DetailView,
+                                  CreateView, UpdateView, 
+                                  DeleteView)
 from django.http import HttpResponse
 from . import models
 # Create your views here.
@@ -21,8 +25,23 @@ class IndexView(TemplateView):
         return context
 
 class SchoolListView(ListView):
+    context_object_name = "schools"
     model = models.School
 
 class SchoolDetailView(DetailView):
+    context_object_name = "school_detail"
     model = models.School
     template_name = 'app/school_detail.html'
+
+class SchoolCreateView(CreateView):
+    fields = ('name', 'principal', 'location')
+    model = models.School
+
+
+class SchoolUpdateView(UpdateView):
+    fields = ('name', 'principal')
+    model = models.School
+
+class SchoolDeleteView(DeleteView):
+    model = models.School
+    success_url = reverse_lazy('app:list')
